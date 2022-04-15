63e22f1bf7a58ab121e3563cc72893283a7479b0
Emma Lichtenstein <emma.lichtenstein@kellogg.northwestern.edu>
2022-04-15 20:53:28 +0000

    creating initial domain model

--- crm/crm.sql
+++ crm/crm.sql
@@ -1,5 +1,39 @@
 -- Drops existing tables, so we start fresh with each
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS activities;
 
 -- CREATE TABLES
+CREATE TABLE users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT
+);
+
+CREATE TABLE contacts(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    title TEXT,
+    phone_number TEXT,
+    email TEXT,
+    company_id INTEGER
+);
+
+CREATE TABLE companies(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    company_name TEXT
+);
+
+CREATE TABLE activities(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    created_at TEXT,
+    message TEXT,
+    activity_type TEXT,
+    user_id INTEGER,
+    contact_id INTEGER
+);
